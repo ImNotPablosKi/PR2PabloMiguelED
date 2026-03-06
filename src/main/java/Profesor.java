9971d32b18759736c0de7f13019c8194526049a4
ImNotPablosKi <pmigueld@iesch.org>
2026-03-06 20:42:13 +0100

    Feature/five: Declarar como final atributos de Profesor.java

--- src/main/java/Profesor.java
+++ src/main/java/Profesor.java
@@ -2,8 +2,8 @@ import java.time.LocalDate;
 
 public class Profesor extends Trabajador {
 
-    private String especialidad;
-    private int horasLectivas;
+    private final String especialidad;
+    private final int horasLectivas;
 
     public Profesor(String nombre, String apellidos, String dni, LocalDate fechaIncorporacion, Double salarioBase, String especialidad, int horasLectivas) {
         super(nombre, apellidos, dni, fechaIncorporacion, salarioBase);
